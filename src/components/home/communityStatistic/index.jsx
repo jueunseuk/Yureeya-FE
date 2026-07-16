08923339cee4c5d8e0f70cccaa47710a32464b72
returns0825 <junsu120202@gmail.com>
2026-07-16 17:39:18 +0900

    temp

--- src/components/home/communityStatistic/index.jsx
+++ src/components/home/communityStatistic/index.jsx
@@ -24,68 +24,68 @@ const CommunityStatistic = () => {
         <S.Wrapper>
             <BC.VerticalWrapper $gap={"3px"}>
                 <BC.Text $size={"16px"} $weight={"600"}>커뮤니티 통계<BC.Icon src={information} style={{marginLeft: "5px"}} $w={"11px"} title="2시간 간격으로 업데이트 됩니다."/></BC.Text>
-                <BC.Text $size={"11px"} $color={"#878787"}>{formatDate(statisticData.createdAt, 4)}</BC.Text>
+                <BC.Text $size={"11px"} $color={"#878787"}>{formatDate(statisticData?.createdAt, 4)}</BC.Text>
             </BC.VerticalWrapper>
             <BC.VerticalWrapper $gap={"5px"}>
                 <S.ItemWrapper>
                     <BC.Text>총 회원 수</BC.Text>
                     <S.DotLine />
-                    <BC.Text>{statisticData.totalMember || 0}</BC.Text>
+                    <BC.Text>{statisticData?.totalMember || 0}</BC.Text>
                 </S.ItemWrapper>
                 <S.ItemWrapper>
                     <BC.Text>오늘 가입한 회원 수</BC.Text>
                     <S.DotLine />
-                    <BC.Text>{statisticData.todayMember || 0}</BC.Text>
+                    <BC.Text>{statisticData?.todayMember || 0}</BC.Text>
                 </S.ItemWrapper>
                 <S.ItemWrapper>
                     <BC.Text>총 게시글 수</BC.Text>
                     <S.DotLine />
-                    <BC.Text>{statisticData.totalPost || 0}</BC.Text>
+                    <BC.Text>{statisticData?.totalPost || 0}</BC.Text>
                 </S.ItemWrapper>
                 <S.ItemWrapper>
                     <BC.Text>오늘 올라온 게시글 수</BC.Text>
                     <S.DotLine />
-                    <BC.Text>{statisticData.todayPost || 0}</BC.Text>
+                    <BC.Text>{statisticData?.todayPost || 0}</BC.Text>
                 </S.ItemWrapper>
                 <S.ItemWrapper>
                     <BC.Text>총 댓글 수</BC.Text>
                     <S.DotLine />
-                    <BC.Text>{statisticData.totalComment || 0}</BC.Text>
+                    <BC.Text>{statisticData?.totalComment || 0}</BC.Text>
                 </S.ItemWrapper>
                 <S.ItemWrapper>
                     <BC.Text>오늘 올라온 댓글 수</BC.Text>
                     <S.DotLine />
-                    <BC.Text>{statisticData.todayComment || 0}</BC.Text>
+                    <BC.Text>{statisticData?.todayComment || 0}</BC.Text>
                 </S.ItemWrapper>
                 <S.ItemWrapper>
                     <BC.Text>총 갤러리 수</BC.Text>
                     <S.DotLine />
-                    <BC.Text>{statisticData.totalGallery || 0}</BC.Text>
+                    <BC.Text>{statisticData?.totalGallery || 0}</BC.Text>
                 </S.ItemWrapper>
                 <S.ItemWrapper>
                     <BC.Text>오늘 올라온 갤러리 수</BC.Text>
                     <S.DotLine />
-                    <BC.Text>{statisticData.todayGallery || 0}</BC.Text>
+                    <BC.Text>{statisticData?.todayGallery || 0}</BC.Text>
                 </S.ItemWrapper>
                 <S.ItemWrapper>
                     <BC.Text>총 유리 변환 횟수</BC.Text>
                     <S.DotLine />
-                    <BC.Text>{statisticData.totalConvert || 0}</BC.Text>
+                    <BC.Text>{statisticData?.totalConvert || 0}</BC.Text>
                 </S.ItemWrapper>
                 <S.ItemWrapper>
                     <BC.Text>오늘 유리 변환된 횟수</BC.Text>
                     <S.DotLine />
-                    <BC.Text>{statisticData.todayConvert || 0}</BC.Text>
+                    <BC.Text>{statisticData?.todayConvert || 0}</BC.Text>
                 </S.ItemWrapper>
                 <S.ItemWrapper>
                     <BC.Text>총 응원 수</BC.Text>
                     <S.DotLine />
-                    <BC.Text>{statisticData.totalCheer || 0}</BC.Text>
+                    <BC.Text>{statisticData?.totalCheer || 0}</BC.Text>
                 </S.ItemWrapper>
                 <S.ItemWrapper>
                     <BC.Text>오늘 응원 수</BC.Text>
                     <S.DotLine />
-                    <BC.Text>{statisticData.todayCheer || 0}</BC.Text>
+                    <BC.Text>{statisticData?.todayCheer || 0}</BC.Text>
                 </S.ItemWrapper>
             </BC.VerticalWrapper>
         </S.Wrapper>
